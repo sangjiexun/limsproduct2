a41976d26783e9efb6859b619aabeee894ddf921
unknown <461524755@qq.com>
2019-05-09 09:44:43 +0800

    补传domain

---
+++ limsproduct/src/main/java/net/zjcclims/domain/AssetCabinetAccessRecord.java
@@ -0,0 +1,135 @@
+package net.zjcclims.domain;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "asset_cabinet_access_record")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(namespace = "zjcclims/net/zjcclims/domain", name = "AssetCabinetAccessRecord")
+public class AssetCabinetAccessRecord implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "id", nullable = false)
+	@Basic(fetch = FetchType.EAGER)
+	@Id
+	@XmlElement
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer id;
+
+	@Column(name = "app_id")
+	@Basic(fetch = FetchType.EAGER)
+	@XmlElement
+	Integer appId;
+
+	@Column(name = "type")
+	@Basic(fetch = FetchType.EAGER)
+	@XmlElement
+	String type;
+
+	@Column(name = "create_date")
+	@Basic(fetch = FetchType.EAGER)
+	@XmlElement
+	Date createDate;
+
+	@Column(name = "asset_id")
+	@Basic(fetch = FetchType.EAGER)
+	@XmlElement
+	Integer assetId;
+
+	@Column(name = "username")
+	@Basic(fetch = FetchType.EAGER)
+	@XmlElement
+	String username;
+
+	@Column(name = "quantity")
+	@Basic(fetch = FetchType.EAGER)
+	@XmlElement
+	Integer quantity;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getAppId() {
+		return appId;
+	}
+
+	public void setAppId(Integer appId) {
+		this.appId = appId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Integer getAssetId() {
+		return assetId;
+	}
+
+	public void setAssetId(Integer assetId) {
+		this.assetId = assetId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (int) (prime * result + ((id == null) ? 0 : id.hashCode()));
+		return result;
+	}
+
+	/**
+	 */
+	public boolean equals(Object obj) {
+		if (obj == this)
+			return true;
+		if (!(obj instanceof AssetCabinetAccessRecord))
+			return false;
+		AssetCabinetAccessRecord equalCheck = (AssetCabinetAccessRecord) obj;
+		if ((id == null && equalCheck.id != null) || (id != null && equalCheck.id == null))
+			return false;
+		if (id != null && !id.equals(equalCheck.id))
+			return false;
+		return true;
+	}
+}
