d95a41a06b5e60162ac0f7b99a7dac5f0a90857b
weicheng <1159736841@qq.com>
2019-05-27 10:42:41 +0800

    开始实验室基础信息微服务

---
+++ limsproduct/src/main/java/net/gvsun/lims/web/labroom/LabRoomController.java
@@ -0,0 +1,79 @@
+package net.gvsun.lims.web.labroom;
+
+import net.zjcclims.dao.SystemTimeDAO;
+import net.zjcclims.domain.SchoolTerm;
+import net.zjcclims.service.common.ShareService;
+import net.zjcclims.service.timetable.OuterApplicationService;
+import net.zjcclims.web.common.PConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/****************************************************************************
+ * Descriptions：实验室预约管理
+ *
+ * @作者：魏诚
+ * @时间：2018-09-04
+ ****************************************************************************/
+@Controller("labRoomController")
+@SessionAttributes("selected_academy")
+@RequestMapping("/lims/labroom/manage")
+public class LabRoomController<JsonResult> {
+    @Autowired
+    private OuterApplicationService outerApplicationService;
+    @Autowired
+    private ShareService shareService;
+    @Autowired
+    private PConfig pConfig;
+    @Autowired
+    SystemTimeDAO systemTimeDAO;
+    /************************************************************
+     * @初始化WebDataBinder，这个WebDataBinder用于填充被@InitBinder注释的处理 方法的command和form对象
+     *
+     ************************************************************/
+    @InitBinder
+    public void initBinder(WebDataBinder binder, HttpServletRequest request) { // Register
+        binder.registerCustomEditor(Calendar.class,
+                new org.skyway.spring.util.databinding.CustomCalendarEditor());
+        binder.registerCustomEditor(byte[].class,
+                new org.springframework.web.multipart.support.ByteArrayMultipartFileEditor());
+        binder.registerCustomEditor(boolean.class, new org.skyway.spring.util.databinding.EnhancedBooleanEditor(false));
+        binder.registerCustomEditor(Boolean.class, new org.skyway.spring.util.databinding.EnhancedBooleanEditor(true));
+        binder.registerCustomEditor(java.math.BigDecimal.class,
+                new org.skyway.spring.util.databinding.NaNHandlingNumberEditor(java.math.BigDecimal.class, true));
+        binder.registerCustomEditor(Integer.class, new org.skyway.spring.util.databinding.NaNHandlingNumberEditor(
+                Integer.class, true));
+        binder.registerCustomEditor(Date.class, new org.skyway.spring.util.databinding.CustomDateEditor());
+        binder.registerCustomEditor(String.class, new org.skyway.spring.util.databinding.StringEditor());
+        binder.registerCustomEditor(Long.class, new org.skyway.spring.util.databinding.NaNHandlingNumberEditor(
+                Long.class, true));
+        binder.registerCustomEditor(Double.class, new org.skyway.spring.util.databinding.NaNHandlingNumberEditor(
+                Double.class, true));
+    }
+
+    /************************************************************
+     * Descriptions：实验室预约管理-可预约的实验室列表页面
+     *
+     * @作者：魏诚
+     * @时间：2019-09-04
+     ************************************************************/
+    @RequestMapping("/labRoomList")
+    public ModelAndView labRoomList(HttpServletRequest request,@ModelAttribute("selected_academy") String acno) {
+        ModelAndView mav = new ModelAndView();
+        List<SchoolTerm> schoolTerms = outerApplicationService.getSchoolTermList();
+        mav.addObject("labRoomMap", outerApplicationService.getLabRoomMap(acno));
+        mav.addObject("zuulServerUrl", pConfig.zuulServerUrl);
+        mav.setViewName("lims/labroom/manage/labRoomList.jsp");
+        return mav;
+    }
+}
